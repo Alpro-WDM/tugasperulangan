21ea76be002365218b0f0d8e3249304f8036121c
wardanimuhamad <44815998+wardanimuhamad@users.noreply.github.com>
2024-10-11 14:00:30 +0700

    Add files via upload

---
+++ Perulangan.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Perulangan {
+    public static void main(String[] args) {
+        int awal, akhir;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("awal: ");
+        awal = sc.nextInt();
+
+        sc.close();
+    }
+}
